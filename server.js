6b37f599c10c4d9586c4914908c3bd914eccc5c0
JackTanRoo <ordo_sagittae@hotmail.com>
2014-10-09 17:32:55 -0700

    Set up server to serve landing page

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require('express');
+var ejs = require('ejs');
+
 var app = express();
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
 
-app.set('views', __dirname + '/public');
+app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
-app.use(express.static(__dirname + '/public/assets'));
-
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
   res.render('index.html');
 });
 
-
 app.listen(port, function(){
-	console.log("on port 3000")
+	console.log("on port 3000");
 });
